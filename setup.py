620e7c28962959d5fd5f85b72477c776003bd433
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2022-03-07 13:56:54 +0100

    add testing and workflows

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bioimageit_formats",
-    version="0.0.2",
+    version="0.0.3",
     author="Sylvain Prigent",
     author_email="sylvain.prigent@inria.fr",
     description="Manage data formats for BioImageIT project",
